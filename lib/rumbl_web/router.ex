3e8417343cec9038d5c8da7ab074875fdbeb6588
Mauricio Chávez <mauriciochavezolea@gmail.com>
2020-12-09 00:42:41 -0600

    Initial commit

---
+++ lib/rumbl_web/router.ex
@@ -0,0 +1,31 @@
+defmodule RumblWeb.Router do
+  use RumblWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", RumblWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
+  end
+
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+
+    scope "/" do
+      pipe_through :browser
+      live_dashboard "/dashboard", metrics: RumblWeb.Telemetry
+    end
+  end
+end
